2a1ca88711c99d62f3e6b49f6ddb02170daf6dea
samuelrodrigues <samuelrodriguessouzaiphone@gmail.com>
2025-08-06 17:57:36 -0300

    feat: added speed next

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={montserrat.className}>
       <body>{children}</body>
+        <SpeedInsights />
     </html>
   );
 }
